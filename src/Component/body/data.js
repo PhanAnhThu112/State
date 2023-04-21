2f6858b45c6f9a6e55130af89a9df540caf38dad
PhanAnhThu112 <116275301+PhanAnhThu112@users.noreply.github.com>
2023-04-21 16:44:26 +0700

    first commit

---
+++ src/Component/body/data.js
@@ -0,0 +1,37 @@
+export function Data() {
+    return [
+      {
+        name: "May Anh 1",
+        image: "images/mayanh1.jpg",
+        type: "M",
+        oldprice: 40000,
+        newprice: 36000,
+        des: "DEP",
+      },
+      {
+        name: "May Anh 1",
+        image: "images/mayanh2.jpg",
+        type: "M",
+        oldprice: 40000,
+        newprice: 36000,
+        des: "DEP",
+      },
+      {
+        name: "May Anh 1",
+        image: "images/mayanh1.jpg",
+        type: "F",
+        oldprice: 50000,
+        newprice: 0,
+        des: "DEP",
+      },
+      {
+        name: "May Anh 1",
+        image: "images/mayanh2.jpg",
+        type: "C",
+        oldprice: 40000,
+        newprice: 36000,
+        des: "DEP",
+      },
+    ];
+  }
+  
